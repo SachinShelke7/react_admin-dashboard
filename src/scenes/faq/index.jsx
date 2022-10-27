d5624765f2b485def3c10df6536962cc11738f0b
Sachin Shelke <shelkesp1@gmail.com>
2022-10-27 13:37:28 +0530

    faq_route-done

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,7 +1,114 @@
+import { ExpandMore } from "@mui/icons-material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import React from "react";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
 
 const FAQ = () => {
-  return <div>Faq</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nostrum,
+            quibusdam obcaecati quia sint modi enim harum inventore ipsam! Ullam
+            reprehenderit illum recusandae, delectus minus impedit debitis animi
+            at facilis.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Another Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nostrum,
+            quibusdam obcaecati quia sint modi enim harum inventore ipsam! Ullam
+            reprehenderit illum recusandae, delectus minus impedit debitis animi
+            at facilis.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Your Fav Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nostrum,
+            quibusdam obcaecati quia sint modi enim harum inventore ipsam! Ullam
+            reprehenderit illum recusandae, delectus minus impedit debitis animi
+            at facilis.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Some Random Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nostrum,
+            quibusdam obcaecati quia sint modi enim harum inventore ipsam! Ullam
+            reprehenderit illum recusandae, delectus minus impedit debitis animi
+            at facilis.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Most Asked Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nostrum,
+            quibusdam obcaecati quia sint modi enim harum inventore ipsam! Ullam
+            reprehenderit illum recusandae, delectus minus impedit debitis animi
+            at facilis.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nostrum,
+            quibusdam obcaecati quia sint modi enim harum inventore ipsam! Ullam
+            reprehenderit illum recusandae, delectus minus impedit debitis animi
+            at facilis.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  );
 };
 
 export default FAQ;
