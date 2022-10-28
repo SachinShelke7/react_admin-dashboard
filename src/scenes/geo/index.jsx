4d02f9dcaa51af27e655b0882249ef2cb7d90aee
Sachin Shelke <shelkesp1@gmail.com>
2022-10-28 15:46:38 +0530

    Geo_CHART

--- src/scenes/geo/index.jsx
+++ src/scenes/geo/index.jsx
@@ -1,7 +1,17 @@
+import { Box } from "@mui/material";
 import React from "react";
+import GeographyChart from "../../components/GeographyChart";
+import Header from "../../components/Header";
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <Box m="20px">
+      <Header title="Geography Chart" subtitle="Simple Geography Chart" />
+      <Box height="75vh" border="1px solid grey" borderRadius="4px">
+        <GeographyChart />
+      </Box>
+    </Box>
+  );
 };
 
 export default index;
